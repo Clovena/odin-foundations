2b0e08cc06de8eb99e1e4638388a7f61ec2d3396
Zachary J Ricker <109747756+Clovena@users.noreply.github.com>
2025-02-17 22:29:46 -0500

    Create js-101 directory and complete first lesson on Javascript in TOP

---
+++ 00-lessons/js-101/javascript.js
@@ -0,0 +1,73 @@
+let fname = "John";
+let surname = "Doe";
+let age = 54;
+const pi = 3.14159;
+
+console.log(fname);
+console.log(surname);
+
+console.log(age);
+age = 14;
+console.log(age);
+
+console.log(pi);
+// pi = 4;
+// console.log(pi);
+
+console.log(3 + 2 - 76 * (1 + 1));
+
+// Below is from javascript.info
+let firstName = "John";
+let admin = firstName;
+// alert(admin);
+console.log(admin);
+planetName = "Earth";
+visitorName = "Zac";
+BIRTHDAY = "01.05.1997";
+// age = function(BIRTHDAY);
+
+// Below is from w3schools
+console.log(100004 % 5);
+let x = 5;
+x++;
+console.log(x); // logs 6
+x--;
+console.log(x); // logs 5
+console.log(x ** 2);
+console.log(Math.pow(x, 2));
+let sciNotation = 123e5;
+console.log(0.2 + 0.1);
+
+// Below is from javascript.info again
+console.log("My " + "string");
+console.log(+true);
+console.log(+"");
+
+let apples = "2";
+let oranges = "3";
+console.log(apples + oranges);
+console.log(+apples + +oranges); // 5
+
+let n = 2;
+n += 5; // 2 + 5 = 7
+n *= 2; // 7 * 2 = 14
+console.log(n);
+n *= 3 + 5; // righthand side evaluated first, then *= operator to finish
+console.log(n);
+
+// "" + 1 + 0  =  "10"
+// "" - 1 + 0  =  -1
+// true + false  =  1
+// 6 / "3"  =  2
+// "2" * "3"  =  6
+// 4 + 5 + "px"  =  "9px"
+// "$" + 4 + 5  =  "$45"
+// "4" - 2  =  2
+// "4px" - 2  =  NaN
+// "  -9  " + 5  =  " -9 5"
+// "  -9  " - 5  =  -14
+// null + 1  =  1
+// undefined + 1  =  NaN
+// " \t \n" - 2  =  -2
+
+console.log("This is the end of the external js sheet.");
