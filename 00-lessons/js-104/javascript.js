0199b3a616be93d753dcc0c18218896d0366a8d0
ZR Zachary Ricker (2749) <zachary.ricker@amgreetings.com>
2025-02-24 15:37:02 -0500

    WIP: js-104

---
+++ 00-lessons/js-104/javascript.js
@@ -0,0 +1,9 @@
+const cars = ["Honda", "Volvo", "BMW"];
+let zacCar = cars[0];
+console.log(zacCar);
+
+cars[2] = "Subaru";
+console.log(cars.toString());
+
+const namedCars = {zac:"Honda", liv:"Volvo", izzi:"Subaru"};
+console.log(namedCars.liv);
