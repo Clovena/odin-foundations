98335e38124740766c47e2dbb67b4dae19b23177
ZR Zachary Ricker (2749) <zjricker@gmail.com>
2025-02-26 15:30:11 -0500

    Code .js for 09_palindromes. Still needs tested.

--- 00-lessons/js-exercises/09_palindromes/palindromes.js
+++ 00-lessons/js-exercises/09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
+const alphaNumeric = function (str) {
+  return str.replace(/[^a-zA-Z0-9]/g, '');
+};
 
+const palindromes = function (str) {
+  let strClean = alphaNumeric(str);
+  let arr = split(strClean, '');
+  return arr === arr.reverse();
 };
 
 // Do not edit below this line
