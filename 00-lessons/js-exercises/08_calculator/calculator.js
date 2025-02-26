327b1ba61c4d56986e61f62eac9957a4a0c9cd82
ZR Zachary Ricker (2749) <zjricker@gmail.com>
2025-02-26 15:24:57 -0500

    Write .js for 08_calculator. Still need to test.

--- 00-lessons/js-exercises/08_calculator/calculator.js
+++ 00-lessons/js-exercises/08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((total, elem) => {
+    return total + elem;
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((total, elem) => {
+    return total * elem;
+  }, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	if (n === 0) {
+    return 1;
+  } else {
+    let result = 1;
+    for (let i = n; i > 0; i--) {
+      result *= i;
+    }
+    return result;
+  }
 };
 
 // Do not edit below this line
