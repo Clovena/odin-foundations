655a681cdbf4099cc6d468515a390e0e74f763a4
ZR Zachary Ricker (2749) <zjricker@gmail.com>
2025-02-26 15:36:08 -0500

    Code js for 10_fibonacci. Still needs tested.

--- 00-lessons/js-exercises/10_fibonacci/fibonacci.js
+++ 00-lessons/js-exercises/10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function (step) {
+  const START_ARRAY = [1, 1];
+  let index = +step - 1;
 
+  if (index < 0) {
+    return 'OOPS';
+  } else if (index === 0) {
+    return 0;
+  } else if (index < 2) {
+    return 1;
+  } else {
+    arr = START_ARRAY;
+    while (index > arr.length) {
+      arr[arr.length] = arr[arr.length - 1] + arr[arr.length - 2];
+    }
+    return arr[arr.length];
+  }
 };
 
 // Do not edit below this line
