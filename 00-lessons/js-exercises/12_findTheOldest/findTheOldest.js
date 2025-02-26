7eb3ff41e5cc0dea7dc45b934a33ef6a24a4827a
ZR Zachary Ricker (2749) <zjricker@gmail.com>
2025-02-26 16:00:09 -0500

    Code js for 12_findTheOldest. Still needs tested.

--- 00-lessons/js-exercises/12_findTheOldest/findTheOldest.js
+++ 00-lessons/js-exercises/12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const getAge = function (birth, death) {
+  if (!death) {
+    death = new Date().getFullYear();
+  }
+  return death - birth;
+};
 
+const findTheOldest = function (obj) {
+  return obj.reduce((older, current) => {
+    const olderAge = getAge(older.yearOfBirth, older.yearOfDeath);
+    const currentAge = getAge(current.yearOfBirth, current.yearOfDeath);
+    return olderAge < currentAge ? current : older;
+  });
 };
 
 // Do not edit below this line
