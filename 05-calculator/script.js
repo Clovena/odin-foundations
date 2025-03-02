539d6d9c2170be9c68985642815cfc8c669978e7
Zachary J Ricker <109747756+Clovena@users.noreply.github.com>
2025-03-01 22:24:41 -0500

    WIP: log bugs, next steps. Start on decimal logic.

--- 05-calculator/script.js
+++ 05-calculator/script.js
@@ -1,45 +1,40 @@
-// Declare operator functions
-function add(a, b) {
-  return a + b;
-}
-function subtract(a, b) {
-  return a - b;
-}
-function multiply(a, b) {
-  return a * b;
-}
-function divide(a, b) {
-  return a / b;
-}
-
 // Declare evaluation function, used within equalsButtom.addEventListener
 function evaluateExpression(left, right, fn) {
+  let a = +left,
+    b = +right;
   if (fn === '+') {
-    return add(left, right);
+    return a + b;
   } else if (fn === '-') {
-    return subtract(left, right);
+    return a - b;
   } else if (fn === 'x') {
-    return multiply(left, right);
+    return a * b;
   } else if (fn === '÷') {
-    return divide(left, right);
+    return a / b;
   } else {
     return 'Error';
   }
 }
 
-// Declare components of the expression
-let expressionLeft = '',
-  expressionRight = '',
-  isLeftSide = true,
-  operation = '';
-
 const miniDisplayValue = document.querySelector('#mini-display');
 const displayValue = document.querySelector('#big-display');
 
+// Declare components of the expression
+let expressionLeft = '',
+  expressionRight = '',
+  operation = '';
+
+// Declare boolean flags to control behavior
+let isLeftSide = true,
+  isEvaluated = false,
+  hasDecimal = false;
+
+// Equals button
 const equalsButton = document.querySelector('#equals');
 equalsButton.addEventListener('click', () => {
   let result = evaluateExpression(expressionLeft, expressionRight, operation);
   miniDisplayValue.textContent += '=';
+
+  isEvaluated = true;
   displayValue.textContent = result;
   expressionLeft = result;
   expressionRight = '';
@@ -47,28 +42,59 @@ equalsButton.addEventListener('click', () => {
   operation = '';
 });
 
+// Numeral buttons
 const numeralButtons = document.querySelectorAll('.numeral');
 numeralButtons.forEach((b) => {
   b.addEventListener('click', () => {
-    displayValue.textContent += b.textContent;
-    miniDisplayValue.textContent += b.textContent;
     // Append numeral value to corresponding side of expression
     if (isLeftSide) {
       expressionLeft += b.textContent;
     } else {
       expressionRight += b.textContent;
     }
+    displayValue.textContent += b.textContent;
+    if (isEvaluated) {
+      miniDisplayValue.textContent = expressionLeft;
+    } else {
+      miniDisplayValue.textContent += b.textContent;
+    }
+    isEvaluated = false;
   });
 });
 
+// Operator buttons
 const operatorButtons = document.querySelectorAll('.operator');
 operatorButtons.forEach((b) => {
   b.addEventListener('click', () => {
     if (operation === '') {
       operation = b.textContent;
-      displayValue.textContent += b.textContent;
-      miniDisplayValue.textContent += b.textContent;
+      displayValue.textContent += operation;
+      if (isEvaluated) {
+        miniDisplayValue.textContent = expressionLeft + operation;
+      } else {
+        miniDisplayValue.textContent += operation;
+      }
       isLeftSide = false;
     }
+    isEvaluated = false;
+    hasDecimal = false;
   });
 });
+
+// Decimal button
+const decimalButton = document.querySelector('#decimal');
+
+
+// CURRENT KNOWN BUGS
+// After evaluating, use of numeral or operator button does not clear
+// mini display, simply concatenates.
+// i.e. need a way to clear mini-display and append result value
+//
+// Can evaluate expressions without
+// one or both of expressionLeft and expressionRight
+//
+// Two consecutive equals signs results in error
+
+// TODO:
+// AC and Del don't do anything yet.
+// Decimal doesn't do anything yet.
